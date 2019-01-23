7b9be12110d6002f84c1b96ba57fafa915a687b5
Maël Semler <leamsem719@hotmail.ca>
2019-01-23 12:37:45 -0500

    FrontEnd put request

--- skedge-frontend/src/App.js
+++ skedge-frontend/src/App.js
@@ -3,23 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: ""};
+  }
+
+  componentDidMount() {
+    axios.put('/localhost/prototype', {"name": "Jon"})
+    .then(res => {
+        this.setState({name: "works"});
+        console.log(res);
+        console.log(res.data);
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        Hello World
       </div>
     );
   }
