98a7c4ad3288ead674f5e4ef799bbd975476d4ba
Jon Zlotnik <j.zlotnik@live.ca>
2019-01-23 13:12:47 -0500

    simple returning of json object backend

--- skedge-backend/index.js
+++ skedge-backend/index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express()
-const port = 80
+const port = 4200
 
 
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.put('/prototype', (req, res) => {
     console.log(req.body)
-    helloObject = JSON.parse(req.body)
-    res.send({"response": helloObject["name"]+", it worked!!"})
+    //ret_obj = {"name": "it worked bro!! -express.js"}
+    res.json(req.body);
+    //res.send({"response": req.body+", it worked!!"})
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
