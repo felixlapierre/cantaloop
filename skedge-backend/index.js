cd85dd3cd0ecb0e519817f42c4254a5a82dd1be5
Jon Zlotnik <j.zlotnik@live.ca>
2019-01-23 12:40:20 -0500

    started backend prototype, may not work

--- skedge-backend/index.js
+++ skedge-backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
-const port = 4200
+const port = 80
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
-//app.put('/helloMongoDB', (req, res)) => 
+app.put('/prototype', (req, res) => {
+    console.log(req.body)
+    helloObject = JSON.parse(req.body)
+    res.send({"response": helloObject["name"]+", it worked!!"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
