276d20ee61427d7cff207cf3b5f1b1d4dda00f2e
Maël Semler <leamsem719@hotmail.ca>
2019-01-23 13:04:32 -0500

    Button to test front/back relation

--- skedge-frontend/src/App.js
+++ skedge-frontend/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { name: ""};
+    this.handleClick = this.handleClick.bind(this);
   }
-
-  componentDidMount() {
+  handleClick() {
+    console.log('Request Sending');
     axios.put('/localhost/prototype', {"name": "Jon"})
     .then(res => {
         this.setState({name: "works"});
@@ -21,6 +22,9 @@ class App extends Component {
     return (
       <div>
         Hello World
+        <button onClick={this.handleClick}>
+          Send Request
+        </button>
       </div>
     );
   }
