f68dd84bfa79f06dcf2b0d6ff6cf9c4a75a3b5ef
MikeBrichko <w.mike.brichko@gmail.com>
2019-02-26 16:34:41 -0500

    removed warnings caused by ide

--- skedge-backend/courses.js
+++ skedge-backend/courses.js
@@ -1,17 +1,17 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var classes = ["SOEN-298","SOEN 422","SOEN 423","SOEN 448","SOEN 491","SOEN 387","SOEN 487","SOEN 228","SOEN 287",
-"SOEN 321","SOEN 331","SOEN 341","SOEN 342","SOEN 343","SOEN 344","SOEN 345","SOEN 357","SOEN 384",
-"SOEN 385","SOEN 390","SOEN 490","COMP 232","COMP 248","COMP 249","COMP 335","COMP 346","COMP 348",
-"COMP 345","COMP 353","COMP 371","COMP 426","COMP 428","COMP 442","COMP 445","COMP 451","COMP 465",
-"COMP 472","COMP 473","COMP 474","COMP 478","COMP 479","COMP 376","COMP 476","COMP 477","COMP 444",
-"COMP 352","BIOL 206","BIOL 261","CHEM 217","CHEM 221","CIVI 231","ELEC 321","ELEC 275","ENGR 242",
-"ENGR 243","ENGR 251","ENGR 361","ENGR 201","ENGR 202","ENGR 213","ENGR 411","ENGR 371","ENGR 391",
-"ENGR 301","MECH 221","PHYS 252","PHYS 284","PHYS 385","AERO 480","AERO 482"];
+const classes = ["SOEN-298", "SOEN 422", "SOEN 423", "SOEN 448", "SOEN 491", "SOEN 387", "SOEN 487", "SOEN 228", "SOEN 287",
+    "SOEN 321", "SOEN 331", "SOEN 341", "SOEN 342", "SOEN 343", "SOEN 344", "SOEN 345", "SOEN 357", "SOEN 384",
+    "SOEN 385", "SOEN 390", "SOEN 490", "COMP 232", "COMP 248", "COMP 249", "COMP 335", "COMP 346", "COMP 348",
+    "COMP 345", "COMP 353", "COMP 371", "COMP 426", "COMP 428", "COMP 442", "COMP 445", "COMP 451", "COMP 465",
+    "COMP 472", "COMP 473", "COMP 474", "COMP 478", "COMP 479", "COMP 376", "COMP 476", "COMP 477", "COMP 444",
+    "COMP 352", "BIOL 206", "BIOL 261", "CHEM 217", "CHEM 221", "CIVI 231", "ELEC 321", "ELEC 275", "ENGR 242",
+    "ENGR 243", "ENGR 251", "ENGR 361", "ENGR 201", "ENGR 202", "ENGR 213", "ENGR 411", "ENGR 371", "ENGR 391",
+    "ENGR 301", "MECH 221", "PHYS 252", "PHYS 284", "PHYS 385", "AERO 480", "AERO 482"];
 
-var myData;
-var filteredJSON;
-for (var i=0; i<classes.length; i++){
+let myData;
+let filteredJSON;
+for (let i=0; i<classes.length; i++){
 	console.log(classes[i].substring(0,4));
 	console.log(classes[i].substring(5));
 	myData = callWebAPI(classes[i].substring(0,4), classes[i].substring(5));
@@ -21,11 +21,11 @@ for (var i=0; i<classes.length; i++){
 }
 
 function callWebAPI(subject, catalog){
-	var userName = "144";
-	var passWord = "d4a992f7c25f4fb53ec1de9f0222a83d";
-	
-	var request = new XMLHttpRequest();
-	request.open("GET", "https://opendata.concordia.ca/API/v1/course/schedule/filter/*/"+ subject +"/" + catalog, false, userName, passWord);
+	const userName = "144";
+    const passWord = "d4a992f7c25f4fb53ec1de9f0222a83d";
+
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://opendata.concordia.ca/API/v1/course/schedule/filter/*/"+ subject +"/" + catalog, false, userName, passWord);
 	request.send();
 	console.log(request.status);
 	
@@ -42,7 +42,7 @@ function retrieveLastThreeSemesterData(myObject, myCriteria){
 }
 
 function removeUnwantedAttributes (filteredJSON){
-	for (var elements in filteredJSON){
+	for (let elements in filteredJSON){
 		delete filteredJSON[elements].courseID;
 		delete filteredJSON[elements].termCode;
 		delete filteredJSON[elements].componentDescription;
