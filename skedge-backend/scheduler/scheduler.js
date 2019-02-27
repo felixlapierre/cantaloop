73a8294d39c3cf849e008a4c7972d892d2e1b198
Felix <felix.lapierre1@gmail.com>
2019-02-27 13:03:25 -0500

    Logic for building schedules in scheduler using all other written modules

--- skedge-backend/scheduler/scheduler.js
+++ skedge-backend/scheduler/scheduler.js
@@ -1,6 +1,13 @@
-var CoursePlacer = require("./course-placement");
-var Requisites = require("./requisites");
-var AddMissingRequisites = require("./add-missing-requisites");
+var CoursePlacer = require("./course-placement/course-placement");
+var Requisites = require("./course-placement/requisites");
+var AddMissingRequisites = require("./course-placement/add-missing-requisites");
+
+var Generation = require("./semester-generation/generation").generation;
+var InitialGeneration = require("./semester-generation/generation").initalGeneration;
+var rankingFunction = require("./semester-generation/fitness").rankGeneration;
+
+const populationLimit = 20;
+const numberOfGenerations = 100;
 
 class Scheduler
 {
@@ -12,6 +19,10 @@ class Scheduler
     GenerateSchedules(courseRecord, courseSequence, semesters)
     {
         var placements = this.GetPlacements(courseRecord, courseSequence, semesters);
+
+        var schedules = GenerateSchedulesFromGenerations(placements);
+
+        return schedules;
     }
 
     GetPlacements(courseRecord, courseSequence, semesters)
@@ -23,5 +34,43 @@ class Scheduler
         var placer = new CoursePlacer(this.catalog, requisites);
 
         return placer.placeCourses(courseRecord, courseSequence, semesters);
+    }
+
+    GenerateSchedulesForSemesters(semesters, placements)
+    {
+        var schedules = [];
+
+        semesters.forEach(semester => {
+
+            var placement = placements[semester.season + " " + semester.year];
+
+            var sectionList = GetSectionList(placement, semester.season);
+
+            var generation = InitialGeneration(placement, sectionList, rankingFunction, populationLimit);
+
+            for(var i = 0; i < numberOfGenerations; i++)
+            {
+                generation = Generation(generation, rankingFunction, sectionList, populationLimit);
+            }
+
+            schedules.push({
+                "year": semester.year,
+                "season": semester.season,
+                "schedules": generation
+            });
+        });
+
+        return schedules;
+    }
+
+    GetSectionList(courses, season)
+    {
+        var sectionList = {};
+
+        courses.forEach(courseId => {
+            sectionList[courseId] = this.catalog[courseId][season];
+        })
+
+        return sectionList;
     }
 }
