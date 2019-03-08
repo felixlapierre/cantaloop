d5d72ee5247cd319d8e53a41f68fe8b9cca3b165
Felix <felix.lapierre1@gmail.com>
2019-03-08 13:04:01 -0500

    Refactored add-missing-prerequisites

--- skedge-backend/scheduler/course-placement/add-missing-requisites.js
+++ skedge-backend/scheduler/course-placement/add-missing-requisites.js
@@ -1,24 +1,40 @@
 function AddMissingPrerequisitesAndCorequisites(courseCatalog, courseSequence, requisites)
 {
     //Regular for loop since course sequence will be extended
+    console.log(courseSequence);
     for(var i = 0; i < courseSequence.length; i++)
     {
-        courseCatalog[courseSequence[i]].prerequisites.forEach(courseId => {
-            if(requisites.status[courseId] === undefined)
-            {
-                requisites.status[courseId] = "Incomplete";
-                courseSequence.push(courseId);
-            }
-        });
-
-        courseCatalog[courseSequence[i]].corequisites.forEach(courseId => {
-            if(requisites.status[courseId] === undefined)
-            {
-                requisites.status[courseId] = "Incomplete";
-                courseSequence.push(courseId);
-            }            
-        });
+        var courseId = courseSequence[i];
+        courseSequence.push(...AddMissingPrerequisites(courseId, courseCatalog, requisites));
+        courseSequence.push(...AddMissingCorequisites(courseId, courseCatalog, requisites));
     }
+}
+
+function AddMissingPrerequisites(courseId, courseCatalog, requisites)
+{
+    var courses = courseCatalog[courseId].prerequisites;
+    return AddAllMissingCoursesFrom(courses, requisites);
+}
+
+function AddMissingCorequisites(courseId, courseCatalog, requisites)
+{
+    var courses = courseCatalog[courseId].corequisites;
+    return AddAllMissingCoursesFrom(courses, requisites);
+}
+
+function AddAllMissingCoursesFrom(courses, requisites)
+{
+    var missingCourses = [];
+    
+    courses.forEach(courseId => {
+        if(requisites.status[courseId] === undefined)
+        {
+            requisites.status[courseId] = "Incomplete";
+            missingCourses.push(courseId);
+        }
+    });
+
+    return missingCourses;
 }
 
 module.exports = AddMissingPrerequisitesAndCorequisites;
