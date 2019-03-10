d3db68906a22188a0a1a2cca4f7586d37bb24042
nguyhh <nguyenhaiha1224@gmail.com>
2019-03-10 17:46:16 -0400

    Function to add a course into database from a json file works

--- skedge-backend/database-service.js
+++ skedge-backend/database-service.js
@@ -1,7 +1,6 @@
 //
 //
-// This module is only for the prototype.
-// It can be deleted by the database team whenever they're ready.
+// Everything related to connecting to MongoDB and manipulating the database
 //
 //
 const mongoose = require('mongoose');
@@ -9,11 +8,12 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const assert = require('assert');
 let courseModel = require('./courseSchem');
-
+fs = require('fs');
 
 
 const server = 'skedge-user:8sDBuOw3zMD4ZpQp@skedge-cantaloop-kueik.mongodb.net';
 const database = 'skedge-app';
+
 
 class Database {
     constructor() {
@@ -32,9 +32,81 @@ class Database {
 
   module.exports = new Database();
 
+    // const db = new Database();
 
 
+/////TESTING with hardcoded course//////////
+///So the following code here works/////////
 
+//Hardcoded course to add to database
+// let course1 = new courseModel (
+//     {
+//         session: "13W",
+//         subject: "BIOL",
+//         catalog: "201",
+//         section: "50",
+//         componentCode: "LEC",
+//         courseTitle: "INTRODUCTORY BIOLOGY",
+//         locationCode: "LOY",
+//         instructionModeCode: "P",
+//         instructionModeDescription: "In Person",
+//         meetingPatternNumber: "1",
+//         roomCode: "CC320",
+//         buildingCode: "CC",
+//         room: "320",
+//         classStartTime: "18.30.00",
+//         classEndTime: "21.00.00",
+//         modays: "N",
+//         tuesdays: "Y",
+//         wednesdays: "N",
+//         thursdays: null,
+//         fridays: "N",
+//         saturdays: "N",
+//         sundays: "N",
+//         classStartDate: "07/05/2014",
+//         classEndDate: "19/08/2014",
+//         career: "Undergraduate",
+//         enrollmentCapacity: "72"
+//     }   
+// )
+// //Saving it into the database
+// course1.save()
+//     .then(doc => {
+//         console.log(doc)
+//     })
+//     .catch( err => {
+//         console.log("errorr")
+//     })
+
+
+ ///////////////////////////////////////////////////////////////////////////////   
+/////// Trying to load a json file, parse it and save it into the database ////
+
+var listCourse = fs.readFileSync('./coursesTesting.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    return(listCourse);
+});
+
+var listConverted = JSON.parse(listCourse);
+
+mongoose.connection.collection('courses').insertMany(listConverted, function( err, result){
+    if(err){
+        console.log("error ca marche pas");
+    }else{
+        console.log("yay reussi");
+    }
+})
+
+// for (i in listConverted){
+//     for( attri in i){
+//         if (typeof(listConverted.columns[i,attri]) != 'undefined'){
+//         var blabla = new courseModel(listConverted.columns[i,attri]);
+//         blabla.save(function (err){
+//             if (err) return console.log("ca pas marcher!!");
+//         })
+//     }
+// }
+// };
 
 
 
@@ -49,48 +121,6 @@ const insertDocuments = function(db, callback){
 		callback(result);
 	});
 }
-
-/////TESTING with hardcoded course//////////
-///So the following code here works/////////
-
-let course1 = new courseModel (
-    {
-        session: "13W",
-        subject: "BIOL",
-        catalog: "201",
-        section: "50",
-        componentCode: "LEC",
-        courseTitle: "INTRODUCTORY BIOLOGY",
-        locationCode: "LOY",
-        instructionModeCode: "P",
-        instructionModeDescription: "In Person",
-        meetingPatternNumber: "1",
-        roomCode: "CC320",
-        buildingCode: "CC",
-        room: "320",
-        classStartTime: "18.30.00",
-        classEndTime: "21.00.00",
-        modays: "N",
-        tuesdays: "Y",
-        wednesdays: "N",
-        thursdays: null,
-        fridays: "N",
-        saturdays: "N",
-        sundays: "N",
-        classStartDate: "07/05/2014",
-        classEndDate: "19/08/2014",
-        career: "Undergraduate",
-        enrollmentCapacity: "72"
-    }   
-)
-course1.save()
-    .then(doc => {
-        console.log(doc)
-    })
-    .catch( err => {
-        console.log("errorr")
-    })
-
 
 
 
