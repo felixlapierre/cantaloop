7fcea6a629a81ad6d6b8eea38d7ceec4112d6731
Yomna Abdelkawy <yabdelkawy@outlook.com>
2019-03-15 04:39:36 -0400

    Hardcoded Schedule is now generated

--- skedge-frontend/src/components/TabContent.js
+++ skedge-frontend/src/components/TabContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Tab, Grid, Container, Header} from 'semantic-ui-react';
+import {Tab, Grid, Container, Header, GridRow} from 'semantic-ui-react';
 import * as ReactDOM from 'react-dom';
 import '../styles/TabContent.css';
 
@@ -7,6 +7,7 @@ class TabContent extends Component{
     constructor(props) {
         super(props);
     }
+    //Functions to create a dynamic schedule instead
     // Monday()
     // {
     //     var classes=[];
@@ -40,7 +41,7 @@ class TabContent extends Component{
   
 
     
- 
+ //The below code generates a hardcoded schedule. Not the best approach.
 render() {
         return (
          <Header as="h2" textAlign="center">   
@@ -48,7 +49,7 @@ render() {
            <br/>
            <br/>
             <Grid centered columns={6}>                         
-             <Grid.Column>
+             <Grid.Column className="meow">
               <h4> Time </h4>
               <Grid.Row className="meow">
                   <h6> 8:30 AM </h6> 
@@ -66,7 +67,10 @@ render() {
               <h6> 10:30 AM</h6>             
                </Grid.Row>
                <Grid.Row className="meow"> 
-              <h6> 11:30 AM</h6>             
+              <h6> 11:00 AM</h6>             
+               </Grid.Row>
+               <Grid.Row className="meow"> 
+              <h6> 11:30 PM</h6>             
                </Grid.Row>
                <Grid.Row className="meow"> 
               <h6> 12:00 PM</h6>             
@@ -93,46 +97,75 @@ render() {
               <h6> 3:30 PM</h6>             
                </Grid.Row>
                <Grid.Row className="meow"> 
-              <h6> 3:30 PM</h6>             
-               </Grid.Row>
-               <Grid.Row className="meow"> 
               <h6> 4:00 PM</h6>             
                </Grid.Row>
                <Grid.Row className="meow"> 
-              <h6> 4:30 PM</h6>                     <h6> COMP 346  <br/> Section S </h6> 
-        
+              <h6> 4:30 PM</h6>             
                </Grid.Row>
 
             </Grid.Column>
             <Grid.Column>
                 <h4> Monday </h4> 
-                <Grid.Row className="meow"> 
-               </Grid.Row>
                 <Grid.Row>               
-                <h6> COMP 346  <br/> Section S </h6> 
+                <h6> COMP 346 Lecture  <br/> Section S (8:30 AM-10:00 AM) </h6> 
                 </Grid.Row>
-                <Grid.Row className="meow"> 
-                <h6> ENGR 371 <br/> </h6>             
+                <br/> <br/> <br/>
+                <Grid.Row> <h6> SOEN 341 Lecture <br/> Section CC (10:15 AM-11:30 AM) </h6>
+                </Grid.Row>
+                <br/> <br/> <br/> <br/> <br/>
+                <Grid.Row> <h6> SOEN 228 Lab <br/> Section E (12:30 PM-2:30 PM) </h6>
+                </Grid.Row>
+              
+            </Grid.Column>
+            <Grid.Column>
+                <h4> Tuesday </h4>   
+               <br/>
+               <br/>
+               <br/>
+               <Grid.Row> 
+               <h6> ENGR 233 Lecture <br/> Section DC (9:15 AM-10:30 AM) </h6>
                </Grid.Row>
+               <br/> <br/> <br/> 
+                <Grid.Row> <h6> SOEN 341 Tutorial <br/> Section CC-A (11:15 AM-12:30 PM) </h6></Grid.Row>
+               
             </Grid.Column>
             <Grid.Column>
-                <h4> Tuesday </h4>
-                <h6> COMP 335  <br/> Section U-U </h6> 
-            </Grid.Column>
-            <Grid.Column>
-                <h4> Wednesday </h4>                
-                <h6> ENGR 213 <br/> Section C-C </h6>
+                <h4> Wednesday </h4>     
+        <Grid.Row>            
+                <h6> ENGR 213 Lecture <br/> Section C-C (8:30 AM-10:00 AM)  </h6></Grid.Row>
+                <br/> <br/> <br/> 
+                <Grid.Row>                
+                     <h6> SOEN 341 Lecture <br/> Section CC (10:15 AM-11:30 AM) </h6>    
+                </Grid.Row>
+                <br/> <br/>
+                <Grid.Row>
+                <h6> SOEN 228 Lecture <br/> Section E (11:45 AM-1:00 PM) </h6>    
+                </Grid.Row>                
+                <br/> <br/> <br/> 
+                <Grid.Row>
+                <h6> COMP 346 Tutorial <br/> Section S-SA (1:30 PM-2:30 PM) </h6>    
+                </Grid.Row> 
 
             </Grid.Column>
             <Grid.Column>
                 <h4> Thursday </h4>
-                <h6> ENGR 233 <br/> Section DC </h6>
+                <br/>
+                <br/>
+                <br/>
+                <Grid.Row>                 
+                    <h6> ENGR 233 Lecture <br/> Section DC (9:15 AM-10:30 AM) </h6>
+                </Grid.Row>
+                <br/> <br/> <br/> 
+                <Grid.Row> <h6> SOEN 341 Tutorial <br/> Section CC-A (11:15 AM-12:30 PM) </h6></Grid.Row>
 
             </Grid.Column>
             <Grid.Column>
-                <h4> Friday </h4>
-                <h6> SOEN 228 <br/> Section E </h6>
-
+            <h4> Friday </h4>                
+                <h6> ENGR 213 Lecture <br/> Section C-C (8:30 AM-10:00 AM) </h6>
+                <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+                <Grid.Row>                
+                     <h6> SOEN 228 Lecture <br/> Section E (11:45 AM-1:00 PM) </h6>    
+                </Grid.Row>
             </Grid.Column>
           </Grid>
           </Header>
