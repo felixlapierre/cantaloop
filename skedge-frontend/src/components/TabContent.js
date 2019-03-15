598b78d881c6522e3a00a5e59687fc6297464ae0
Yomna Abdelkawy <yabdelkawy@outlook.com>
2019-03-14 22:02:46 -0400

    Scheduler using Semantic UI

--- skedge-frontend/src/components/TabContent.js
+++ skedge-frontend/src/components/TabContent.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
-import {Tab} from 'semantic-ui-react';
+import {Tab, Grid, Container, Header} from 'semantic-ui-react';
 import * as ReactDOM from 'react-dom';
-import { ViewState } from "@devexpress/dx-react-scheduler";
-import {
-  Scheduler,
-  WeekView,
-  Appointments
-} from "@devexpress/dx-react-scheduler-material-ui";
+
 
 class TabContent extends Component{
     constructor(props) {
         super(props);
-        this.state = {data: this.props.scheduleComponents};
     }
     
  
 render() {
-    const { classes } = this.state;
         return (
-            // <Tab.Pane>
-            //     <h1> Your schedule for {this.props.scheduleComponents[0]}</h1>
-            // </Tab.Pane>
-              <Scheduler classes={classes}>
-                <ViewState currentDate="2018-06-28" />
-                <WeekView startDayHour={9} endDayHour={19} />
-                <Appointments />
-              </Scheduler>
+         <Header as="h2" textAlign="center">   
+           Days of Week
+           <br/>
+           <br/>
+            <Grid centered columns={6}>                         
+             <Grid.Column>
+              <h4> Time </h4>
+            </Grid.Column>
+            <Grid.Column>
+                <h4> Monday </h4>
+            </Grid.Column>
+            <Grid.Column>
+                <h4> Tuesday </h4>
+            </Grid.Column>
+            <Grid.Column>
+                <h4> Wednesday </h4>
+            </Grid.Column>
+            <Grid.Column>
+                <h4> Thursday </h4>
+            </Grid.Column>
+            <Grid.Column>
+                <h4> Friday </h4>
+            </Grid.Column>
+          </Grid>
+          </Header>
         )
     }
 }
