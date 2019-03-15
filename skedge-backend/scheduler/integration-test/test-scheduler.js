1f10270718d228581c2b9cf50929f9acfa71780e
felixlapierre1 <32281260+felixlapierre1@users.noreply.github.com>
2019-03-14 22:19:49 -0400

    Scheduler/tests (#20)
    * Added missing assert
    
    * Added more test courses sections to the courses.json object
    
    * Added tests for scheduler with course record, prerequisites

--- skedge-backend/scheduler/integration-test/test-scheduler.js
+++ skedge-backend/scheduler/integration-test/test-scheduler.js
@@ -6,6 +6,21 @@ var fs = require('fs');
 var catalog = JSON.parse(fs.readFileSync(__dirname + "./../test/courses.json")).courses;
 var scheduler = new Scheduler(catalog);
 
+var twoSemesters = [
+    {
+        "season": "fall",
+        "year":"2019",
+        "credits":15,
+        "numCourses":5
+    },
+    {
+        "season": "winter",
+        "year": "2020",
+        "credits":15,
+        "numCourses":5
+    }
+];
+
 describe('createSchedules', () => {
     it('should create an empty schedule when provided an empty course sequence', () => {
         //Arrange
@@ -51,5 +66,53 @@ describe('createSchedules', () => {
         firstSemester.schedules.forEach(schedule => {
             expect(schedule[someClass]).to.not.be.undefined;
         })
+    })
+    it('should place a class in the first semester if its prerequisites are taken', () => {
+        //Arrange
+        var someCourse = "COMP248";
+        var someCourseWithPrereq = "COMP249";
+
+        var someCourseRecord = [someCourse];
+        var someCourseSequenceWithPrerequisite = [someCourseWithPrereq];
+        var someSemesters = twoSemesters;
+
+        //Act
+        var schedules = scheduler.GenerateSchedules(someCourseRecord, someCourseSequenceWithPrerequisite, someSemesters);
+
+        //Assert
+        var firstSemester = schedules[0];
+        var secondSemester = schedules[1];
+
+        firstSemester.schedules.forEach(schedule => {
+            expect(schedule[someCourseWithPrereq]).to.not.be.undefined;
+            expect(schedule[someCourse]).to.be.undefined;
+        })
+        expect(secondSemester.schedules).to.be.empty;
+    })
+
+    it('should place a class in the second semester if its prerequisite is not taken', () => {
+        //Arrange
+        var someCourseWithPrereq = "COMP249";
+        var prerequisiteCourse = "COMP248";
+
+        var someCourseRecord = [];
+        var someCourseSequenceWithPrerequisite = [someCourseWithPrereq];
+        var someSemesters = twoSemesters;
+
+        //Act
+        var schedules = scheduler.GenerateSchedules(someCourseRecord, someCourseSequenceWithPrerequisite, someSemesters);
+
+        //Assert
+        var firstSemester = schedules[0];
+        var secondSemester = schedules[1];
+
+        firstSemester.schedules.forEach(schedule => {
+            expect(schedule[prerequisiteCourse]).to.not.be.undefined;
+            expect(schedule[someCourseWithPrereq]).to.be.undefined;
+        })
+        secondSemester.schedules.forEach(schedule => {
+            expect(schedule[prerequisiteCourse]).to.be.undefined;
+            expect(schedule[someCourseWithPrereq]).to.not.be.undefined;
+        });
     })
 })
