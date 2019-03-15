d939ddb8a61c7c1ba75388121d3d440da9539553
Felix <felix.lapierre1@gmail.com>
2019-03-15 01:08:43 -0400

    Wrote a failing test that tries to acquire courseCatalog using db

---
+++ skedge-backend/scheduler/integration-test/test-scheduler-db.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var Scheduler = require('./../scheduler');
+var db = require('./../../database/database-service');
+
+describe('createSchedules', () => {
+    it ('should generate schedules when using catalog from db', () => {
+        var catalog = db.getCourseCatalog();
+
+        expect(catalog).to.not.be.undefined;
+    })
+})
