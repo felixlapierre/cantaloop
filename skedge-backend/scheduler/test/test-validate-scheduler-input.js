90b89e725a348c63889f9624e20cb86523e8cda2
Felix <felix.lapierre1@gmail.com>
2019-03-14 20:35:24 -0400

    Added more tests for validating semesters object input

--- skedge-backend/scheduler/test/test-validate-scheduler-input.js
+++ skedge-backend/scheduler/test/test-validate-scheduler-input.js
@@ -2,16 +2,6 @@ var expect = require('chai').expect;
 var validator = require('./../validate-scheduler-input');
 
 describe('ValidateParameterIsArrayOfCourseIds', () => {
-    before(() => {
-        someValidSemester = {
-            year: 5,
-            season: "fall",
-            credits: 15,
-            numCourses: 5,
-            restrictions: []
-        };
-    })
-
     it('should produce an error when passed a non-array course record', () => {
         //Arrange
         var someObject = {};
@@ -36,14 +26,55 @@ describe('ValidateParameterIsArrayOfCourseIds', () => {
         expect(() => validator.ValidateParameterIsArrayOfCourseIds(someCourseRecordContainingBadlyFormattedString, "parameterName")).to.throw("SchedulerInputFormatException");
     })
 })
+var someValidSemester;
 
 describe('ValidateParameterIsArrayOfSemesters', () => {
+    before(() => {
+        someValidSemester = {
+            year: 5,
+            season: "fall",
+            credits: 15,
+            numCourses: 5,
+            restrictions: []
+        };
+    });
+
+    it('should produce no error when passed a valid semester object', () => {
+        //Arrange
+        var someSemesters = [someValidSemester];
+        
+        //Act & Assert
+        validator.ValidateParameterIsArrayOfSemesters(someSemesters, "parameterName");
+    })
     it('should produce an error when passed a semester with a non-number year', () => {
         //Arrange
         var someSemesterWithBadYear = someValidSemester;
-        someSemesterWithBadYear.year = "5";
+        someSemesterWithBadYear.year = "SomeNonNumberString";
+
+        var someSemesters = [someSemesterWithBadYear];
+        
+        //Act & Assert
+        expect(() => validator.ValidateParameterIsArrayOfSemesters(someSemesters, "parameterName")).to.throw("SchedulerInputFormatException");
+    })
+
+    it('should produce an error when passed a semester with non-number credits', () => {
+        //Arrange
+        var someSemesterWithBadCredits = someValidSemester;
+        someSemesterWithBadCredits.credits = "SomeNonNumberString";
+
+        var someSemesters = [someSemesterWithBadCredits]
 
         //Act & Assert
-        expect(() => validator.ValidateParameterIsArrayOfSemesters(someSemesterWithBadYear, "parameterName")).to.throw("SchedulerInputFormatException");
+        expect(() => validator.ValidateParameterIsArrayOfSemesters(someSemesters, "parameterName")).to.throw("SchedulerInputFormatException");
+    })
+
+    it('should produce an error when passed a semester with non-number numCourses', () => {
+        //Arrange
+        var someSemesterWithBadNumCourses = someValidSemester;
+        someSemesterWithBadNumCourses.numCourses = "SomeNonNumberString";
+
+        var someSemesters = [someSemesterWithBadNumCourses];
+
+        expect(() => validator.ValidateParameterIsArrayOfSemesters(someSemesters, "parameterName")).to.throw("SchedulerInputFormatException");
     })
 })
