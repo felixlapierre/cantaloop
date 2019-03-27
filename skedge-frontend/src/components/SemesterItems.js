2231898be2eee26503ea1b81a82a3b8b1317fb38
Brianne O'Gallagher-Roy <brianne13roy@live.com>
2019-03-27 12:27:27 -0400

    semesters object is created, needs link

--- skedge-frontend/src/components/SemesterItems.js
+++ skedge-frontend/src/components/SemesterItems.js
@@ -15,36 +15,47 @@ class SemesterItems extends Component {
       semesters : []
     }
     this.handleAddSemester = this.handleAddSemester.bind(this);
-    this.handleShareholderNameChange = this.handleShareholderNameChange.bind(this);
-    this.handleRemoveShareholder = this.handleRemoveShareholder.bind(this);
-    this.handleSemesterNameChange = this.handleSemesterNameChange.bind(this);
+    this.handleSemesterYearChange = this.handleSemesterYearChange.bind(this);
+    this.handleSemesterSeasonChange = this.handleSemesterSeasonChange.bind(this);
+    this.handleSemesterCreditsChange = this.handleSemesterCreditsChange.bind(this);
+    this.handleRemoveSemester = this.handleRemoveSemester.bind(this);
     this.handleCheckState = this.handleCheckState.bind(this);
   }
 
-  handleShareholderNameChange(index, event) {
-    console.log(index + " changed");
-    console.log(event.target.value);
-    console.log("before: " + this.state.semesters[index].year);
+  handleSemesterYearChange(index, event) {
     var newSemesters = this.state.semesters;
     newSemesters[index].year = event.target.value;
-    console.log("after: " + newSemesters[index].year);
     this.setState({
         semesters: newSemesters
     })
   }
 
-  handleSemesterNameChange(index, event){
-    console.log("index: " + index);
-    console.log("event.target.value: " + event.target.value)
-    console.log("radio clicked, value of semesterName is " )
+  handleSemesterSeasonChange(index, event){
+    console.log("season change");
     var newSemesters = this.state.semesters;
-  //  newSemesters[index].semesterName = value;
+    newSemesters[index].season = event.target.innerText;
     this.setState({
-      //semesters: newSemesters
+      semesters: newSemesters
     })
   }
 
-  handleRemoveShareholder(index) {
+  handleSemesterCreditsChange(index, event){
+    var newSemesters = this.state.semesters;
+    newSemesters[index].credits = event.target.value;
+    this.setState({
+        semesters: newSemesters
+    })
+  }
+
+  handleSemesterNumCoursesChange(index, event) {
+    var newSemesters = this.state.semesters;
+    newSemesters[index].numCourses = event.target.value;
+    this.setState({
+        semesters: newSemesters
+    })
+  }
+
+  handleRemoveSemester(index) {
     console.log(index + " remove");
     var newSemesters = [];
     for(var i = 0; i < this.state.semesters.length; i++){
@@ -63,7 +74,7 @@ class SemesterItems extends Component {
   handleAddSemester(event){
     event.preventDefault();
     console.log("add semester");
-    const newSemesters = this.state.semesters.concat([{ year: '' }])
+    const newSemesters = this.state.semesters.concat([{ year: '2019', season: 'Fall', credits: '15', numCourses: '5', restrictions: []}])
     this.setState({
       semesters: newSemesters
     });
@@ -79,38 +90,56 @@ class SemesterItems extends Component {
     return (
       <form>
       {this.state.semesters.map((semester, index) => (
-          <div className="semesterObject" key={index}>
-            <Input placeholder={`Year`}
-            value={semester.year}
-            onChange={this.handleShareholderNameChange.bind(this, index)}
-            label="Year"
+            <div className="semesterObject" key={index}>
+            <Radio
+            label='Fall'
+            name={"group" + index.toString()}
+            value='Fall'
+            checked={semester.season === 'Fall'}
+            onChange={this.handleSemesterSeasonChange.bind(this, index)}
             />
             <br/>
             <Radio
             label='Winter'
             name={"group" + index.toString()}
             value='Winter'
-            checked={semester.semesterName === 'Winter'}
-            onChange={this.handleSemesterNameChange.bind(this, index)}
-            />
-            <br/>
-            <Radio
-            label='Fall'
-            name={"group" + index.toString()}
-            value='Fall'
-            checked={semester.semesterName === 'Fall'}
-            onChange={this.handleSemesterNameChange.bind(this, index)}
+            checked={semester.season === 'Winter'}
+            onChange={this.handleSemesterSeasonChange.bind(this, index)}
             />
             <br/>
             <Radio
             label='Summer'
             name={"group" + index.toString()}
             value='Summer'
-            checked={semester.semesterName === 'Summer'}
-            onChange={this.handleSemesterNameChange.bind(this, index)}
+            checked={semester.season === 'Summer'}
+            onChange={this.handleSemesterSeasonChange.bind(this, index)}
             />
             <br/>
-            <Button type="button" onClick={this.handleRemoveShareholder.bind(this, index)}>
+            <Input placeholder={`Year`}
+            type='number'
+            value={semester.year}
+            onChange={this.handleSemesterYearChange.bind(this, index)}
+            label="Year"
+            />
+            <br/>
+            <br/>
+            <Input
+            placeholder={`Number of Credits`}
+            type='number'
+            value={semester.credits}
+            onChange={this.handleSemesterCreditsChange.bind(this, index)}
+            label="Credits"
+            />
+            <br/>
+            <br/>
+            <Input placeholder={`Number of Courses`}
+            type='number'
+            value={semester.numCourses}
+            onChange={this.handleSemesterNumCoursesChange.bind(this, index)}
+            label="Courses"
+            />
+            <br/>
+            <Button type="button" onClick={this.handleRemoveSemester.bind(this, index)}>
               -
             </Button>
           </div>
