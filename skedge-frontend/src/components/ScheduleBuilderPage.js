4d509c4cf14d858836c76bd603e21434311d3ffd
Stromation <leamsem719@hotmail.ca>
2019-03-29 13:38:42 -0400

    Frontend/modify sequence on schedule (#37)
    * Added Grid to ScheduleBuilderPage and some CSS stuff
    
    * Added Dropdown to search classes, re-wrote the linking process for the frontend
    
    * Stash changes
    
    * Added list with remove list item and regenerate schedule
    
    * removed a conflict which slipped through my vigilent eyes because it is late
    
    * Rewrote router for frontend and class list for user

--- skedge-frontend/src/components/ScheduleBuilderPage.js
+++ skedge-frontend/src/components/ScheduleBuilderPage.js
@@ -3,27 +3,33 @@ import '../styles/ScheduleBuilderPage.css';
 import Schedule from './Schedule';
 import HeaderPage from './HeaderPage.js';
 import TabContent from './TabContent.js';
-import { Icon, Menu, Segment, Sidebar, Tab } from 'semantic-ui-react';
+import { Icon, Menu, Dropdown, List, Grid, Segment, Sidebar, Tab} from 'semantic-ui-react';
+import axios from "axios";
 
 //The main page after a user logs in
 class ScheduleBuilderPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {visible: false};
+    this.state = {visible: false,
+                  currentClasses:[]
+                  };
     this.panes = [];
     this.scheduleComponents = [];
     this.handleHamburgerButton = this.handleHamburgerButton.bind(this);
+    this.listItemClicked = this.listItemClicked.bind(this);
   }
 
   handleHamburgerButton(){
       this.setState((state) => {
         return {visible: !this.state.visible};
       });
-    console.log(this.state.visible);
   }
 
+
   componentWillMount(){
+    this.setState({currentClasses: JSON.parse(window.sessionStorage.getItem('courseSequence'))});
+
     var years = {};
     this.props.scheduleGiven.forEach(element => {
       var year = element.year;
@@ -43,36 +49,77 @@ class ScheduleBuilderPage extends Component {
     }
   }
 
+  listItemClicked(event){
+    console.log(event);
+    var temp = this.state.currentClasses.filter(function(ele){
+             return ele !== event;
+    });
+    this.setState({currentClasses: temp});
+    axios.post('/genSchedules', this.state.currentClasses).then(response => {
+      console.log("Received: ");
+      console.log(response.data);
+    });
+  }
+
   paneRender(){
     return (<Tab.Pane><TabContent scheduleComponents={this.scheduleComponents} scheduleGiven={this.props.scheduleGiven}/></Tab.Pane>)
   }
 
   render() {
+    const Children = this.state.currentClasses.map((child) =>
+          <List.Item className="child-list-item" key={child.key} onClick={() => this.listItemClicked(child)}>{child.text}</List.Item>);
+
+
     return (
       <div>
         <HeaderPage />
-        <div>
-          <Icon id='hamburgerButton' name='bars' size='big' onClick={this.handleHamburgerButton} />
-          <Sidebar.Pushable as={Segment}>
-            <Sidebar
-              as={Menu}
-              animation='overlay'
-              icon='labeled'
-              inverted
-              onHide={this.handleSidebarHide}
-              vertical
-              visible={this.state.visible}
-            >
-              <Menu.Item as='a'>Hamburger</Menu.Item>
-            </Sidebar>
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            dimmed={'true'}
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={this.state.visible}
+            width= 'thin'
+          >
+            <Menu.Item as='a'>Hamburger</Menu.Item>
+          </Sidebar>
 
-            <Sidebar.Pusher>
-              <Segment basic>
-                <Tab panes={this.panes} />
-              </Segment>
-            </Sidebar.Pusher>
-          </Sidebar.Pushable>
-        </div>
+          <Sidebar.Pusher  dimmed={this.state.visible} onClick={this.handleHamburgerButton}>
+            <Grid id='scheduleGrid' padded>
+              <Grid.Row id='scheduleGridRow'>
+                <Grid.Column width={16}>
+                <Icon id='hamburgerButton' name='bars' size='big' onClick={this.handleHamburgerButton} />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row id='sidebarFullPage'>
+                <Grid.Column width={4}>
+                  <Dropdown
+                      placeholder = 'Search Course'
+                      fluid
+                      search
+                      selection
+                      options = {this.state.currentClasses}
+                      id='dropdownCourses'
+                  />
+                  <div id='coursesTaking'>
+                    <List divided relaxed>
+                      {Children}
+                    </List>
+                  </div>
+                </Grid.Column>
+                <Grid.Column width={12}>
+                  <Tab panes={this.panes} />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+
+
       </div>
     );
   }
