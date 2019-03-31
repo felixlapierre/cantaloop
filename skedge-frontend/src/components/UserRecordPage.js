0e37ac4b94e1899e1a0ff382e8cb3d23b39d64cc
Brianne O'Gallagher-Roy <brianne13roy@live.com>
2019-03-30 23:12:32 -0400

    validation

--- skedge-frontend/src/components/UserRecordPage.js
+++ skedge-frontend/src/components/UserRecordPage.js
@@ -25,7 +25,6 @@ class UserRecordPage extends Component {
     this.deleteCourseItem = this.deleteCourseItem.bind(this);
     this.formatRecordAndCourseSequence = this.formatRecordAndCourseSequence.bind(this);
     this.handleCourseSubmission = this.handleCourseSubmission.bind(this);
-    this.validateSubmission = this.validateSubmission.bind(this);
   }
 
   componentDidMount() {
@@ -161,40 +160,8 @@ class UserRecordPage extends Component {
     }
   }
 
-  validateSubmission(coursesPayload){
-    var errorString = '';
-    var problem = false;
-    if(coursesPayload.courseSequence.length == 0){
-      errorString += "Add Courses to Course Sequence\n";
-      problem = true;
-    }
-    if(coursesPayload.semesters.length == 0){
-      errorString += "Add Semesters\n";
-      problem = true;
-    }
-    var validSemesterObject = true;
-    for (var i in coursesPayload.semesters){
-      if(coursesPayload.semesters[i].year === '' ||
-          coursesPayload.semesters[i].numCourses === '0' ||
-          coursesPayload.semesters[i].credits === '0') {
-        validSemesterObject = false;
-        break;
-      }
-    }
-    if(!validSemesterObject) {
-      errorString += "Enter valid Semesters\n";
-      problem = true;
-    }
-    if(problem){
-      alert(errorString)
-    }
-  }
-
   handleCourseSubmission(){
     let coursesPayload = this.formatRecordAndCourseSequence();
-    if(!this.validateSubmission(coursesPayload)){
-      return;
-    }
     console.log("sending:")
     console.log("courseRecord: " + coursesPayload.courseRecord);
     console.log("course sequence: " + coursesPayload.courseSequence);
@@ -220,7 +187,7 @@ class UserRecordPage extends Component {
       <div className = "outer">
           <h3 className = "welcome-title" >
               <br/>
-              Hi! Welcome to <h2 className="skedge">Skedge</h2>
+              Hi! Welcome to Skedge
               <br/>
               <br/>
           </h3>
@@ -271,6 +238,8 @@ class UserRecordPage extends Component {
                 <SemesterItems semesters={this.state.semesters} handleUpdateSemesters={(semesters) => this.setState({semesters})}/>
                 </div>
             </div>
+          <div>
+          </div>
           <Button id = "goToScheduleBuilder" onClick = {this.handleCourseSubmission}>Make My Schedule</Button>
       </div>
     );
