26aa77185ef574c1e5c2dc1694e6c5777b29f769
Mike Brichko <w.mike.brichko@gmailcom>
2019-04-02 18:37:18 -0400

    added semesters attribute to userRecordSequenceSchema

--- skedge-backend/database/schemas/userRecordSequenceSchema.js
+++ skedge-backend/database/schemas/userRecordSequenceSchema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userRecordSequenceSchema = mongoose.Schema({
     courseRecord: {type: Array, required: true},
     courseSequence: {type: Array, required: true},
+    semesters: {type: Array, required:true},
     creator: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
 });
 
